9f242c40c9aec0f99eb0ba0efb778134e5f9a592
AB <asbigb@gmail.com>
2023-02-18 12:01:32 +0000

    added custom js

--- .github/actions/deploy-s3-javascript/master.js
+++ .github/actions/deploy-s3-javascript/master.js
@@ -3,7 +3,7 @@ const exec = require('@actions/exec');
 const github = require('@actions/github');
 
 function run() {
-    core.notice('Hello from my custom JS actions')
+    core.notice('Hello from my custom JS actions');
 }
 
 run();
